e613728abc5f5c259f0024ed52f5c982ce193337
jp <jp.mariano@newscycle.com>
2015-04-16 17:19:59 +0800

    updates

--- NewsdayMetadataMask/src/com/atex/h11/newsday/metadata/common/DataSource.java
+++ NewsdayMetadataMask/src/com/atex/h11/newsday/metadata/common/DataSource.java
@@ -26,44 +26,47 @@ public class DataSource {
 	public static NCMDataSource getDataSource(ConfigModel config) 
 			throws FileNotFoundException, IOException, CustomException {
 		logger.entering(NCMDataSource.class.getSimpleName(), "getDataSource");
-		DataSourceManager dsmgr = null;
 		
-		Properties connectProps = new Properties();
-		connectProps.load(new FileInputStream(config.getConnectionPropertiesFile()));
-
-        Enumeration<?> connectEnum = connectProps.propertyNames();
-        while (connectEnum.hasMoreElements()) {
-        	String key = (String) connectEnum.nextElement();
-        	String value = connectProps.getProperty(key);
-        	System.setProperty(key, value);
-        	logger.finer("connect: " + key + "=" + value);
-        }    		
-				
-		Properties jndiProps = new Properties();
-        jndiProps.load(new FileInputStream(System.getProperty(Constants.JNDI_PROPERTIES)));
-        
-        // Get a DataSourceManager instance.
-        dsmgr = DataSourceManager.getInstance(jndiProps);
-        
-        Enumeration<?> jndiEnum = jndiProps.propertyNames();
-        while (jndiEnum.hasMoreElements()) {
-        	String key = (String) jndiEnum.nextElement();
-        	logger.finer("jndi: " + key + "=" + jndiProps.getProperty(key));
-        }                    	  
-        
-		String user = config.getAPIUser();
-		String password = config.getAPIPassword();
-		logger.finer("API user=" + user + ", password=" + password);
-		
-		// Login
-		UPSUser upsUser = 
-			UPSUser.instanceUPSUserForNamedUser(user, password, ApplicationConstants.APP_MEDIA_API_ID);
-		ds = (NCMDataSource) dsmgr.getDataSource(NCMDataSource.DS_PK, upsUser, ApplicationConstants.APP_MEDIA_API_ID);		
-	
 		if (ds == null) {
-			throw new CustomException("Datasource is null");
-		} else {
-			logger.finer("Datasource initialized");
+			DataSourceManager dsmgr = null;
+			
+			Properties connectProps = new Properties();
+			connectProps.load(new FileInputStream(config.getConnectionPropertiesFile()));
+	
+	        Enumeration<?> connectEnum = connectProps.propertyNames();
+	        while (connectEnum.hasMoreElements()) {
+	        	String key = (String) connectEnum.nextElement();
+	        	String value = connectProps.getProperty(key);
+	        	System.setProperty(key, value);
+	        	logger.finer("connect: " + key + "=" + value);
+	        }    		
+					
+			Properties jndiProps = new Properties();
+	        jndiProps.load(new FileInputStream(System.getProperty(Constants.JNDI_PROPERTIES)));
+	        
+	        // Get a DataSourceManager instance.
+	        dsmgr = DataSourceManager.getInstance(jndiProps);
+	        
+	        Enumeration<?> jndiEnum = jndiProps.propertyNames();
+	        while (jndiEnum.hasMoreElements()) {
+	        	String key = (String) jndiEnum.nextElement();
+	        	logger.finer("jndi: " + key + "=" + jndiProps.getProperty(key));
+	        }                    	  
+	        
+			String user = config.getAPIUser();
+			String password = config.getAPIPassword();
+			logger.finer("API user=" + user + ", password=" + password);
+			
+			// Login
+			UPSUser upsUser = 
+				UPSUser.instanceUPSUserForNamedUser(user, password, ApplicationConstants.APP_MEDIA_API_ID);
+			ds = (NCMDataSource) dsmgr.getDataSource(NCMDataSource.DS_PK, upsUser, ApplicationConstants.APP_MEDIA_API_ID);		
+		
+			if (ds == null) {
+				throw new CustomException("Datasource is null");
+			} else {
+				logger.finer("Datasource initialized");
+			}
 		}
 		
 		logger.exiting(NCMDataSource.class.getSimpleName(), "getDataSource");
