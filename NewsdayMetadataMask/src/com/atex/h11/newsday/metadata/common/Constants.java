3ebf18c947edf3a614982060126d532a80c48025
jp <jp.mariano@newscycle.com>
2015-04-23 06:09:32 +0800

    constants updates

--- NewsdayMetadataMask/src/com/atex/h11/newsday/metadata/common/Constants.java
+++ NewsdayMetadataMask/src/com/atex/h11/newsday/metadata/common/Constants.java
@@ -11,20 +11,20 @@ public interface Constants {
 	public static final String LOGFILE_NAME = "NewsdayCustomMetadata.log";
 	public static final String LOGGER_NAME = CustomMetadataPanel.class.getName();
 	public static final Logger LOGGER = Logger.getLogger(LOGGER_NAME);
-	public static final boolean STANDALONE_DEBUG = (System.getenv("DEBUG") != null && System.getenv("DEBUG").equals("1"));
 	
 	//
-	// Common constants
+	// Dir constants
 	//
+	public static final boolean STANDALONE_DEBUG = (System.getenv("DEBUG") != null && System.getenv("DEBUG").equals("1"));
 	public static final String SYS_HERMES_DIR_VARIABLE = "HermesDir";
 	public static final String SYS_HERMES_DIR_DEFAULT = STANDALONE_DEBUG ? System.getenv(SYS_HERMES_DIR_VARIABLE) : "c:/Hermes11/";
-	public static final String SYS_HERMES_DIR = System.getProperty(SYS_HERMES_DIR_VARIABLE, Constants.SYS_HERMES_DIR_DEFAULT);
+	public static final String SYS_HERMES_DIR = System.getProperty(SYS_HERMES_DIR_VARIABLE, SYS_HERMES_DIR_DEFAULT);
 
 	// 
 	// File Path constants
-	// The path is relative to the HermesDir folder, i.e. the folder containing nroom.exe
-	public static final String METADATA_GUI_PATH = "c:\\Hermes11\\custommetadata\\";
-	public static final String METADATA_PROPERTIES_FILE = METADATA_GUI_PATH + "NewsdayMetadata.properties";
+	public static final String METADATA_PROPERTIES_DIR_DEFAULT = "c:\\Hermes11\\custommetadata\\"; 
+	public static final String METADATA_PROPERTIES_DIR = System.getProperty("MetadataPropertiesDir", METADATA_PROPERTIES_DIR_DEFAULT);
+	public static final String METADATA_PROPERTIES_FILE = METADATA_PROPERTIES_DIR + "NewsdayMetadata.properties";
 	public static final String METADATA_CONFIG_FILE_PROPERTY = "MetadataConfigFile";
 	
 	public static final String SECURITY_PROPERTIES = "SecurityProperties";
