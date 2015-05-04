e0a2e9dc22fc2f2b01574719a8ffa9a55c19b4aa
jp <jp.mariano@newscycle.com>
2015-05-05 00:13:10 +0800

    batch file update

---
+++ NewsdayMetadataMask/src/main/scripts/copy_to_hermes.bat
@@ -0,0 +1,3 @@
+copy C:\GitRepos\Newsday\NewsdayMetadataMask\NewsdayMetadataMask\target\newsdaymetadata-jar-with-dependencies.jar c:\Hermes11\custommetadata\newsdaymetadata.jar
+copy C:\GitRepos\Newsday\NewsdayMetadataMask\NewsdayMetadataMask\src\main\custommetadata\newsdaymetadata.xml c:\Hermes11\custommetadata\
+pause
--- NewsdayMetadataMask/src/main/scripts/copy_to_hermes_dir.bat
+++
@@ -1,3 +0,0 @@
-copy C:\GitRepos\Newsday\NewsdayMetadataMask\NewsdayMetadataMask\target\newsdaymetadata.jar c:\Hermes11\custommetadata\
-copy C:\GitRepos\Newsday\NewsdayMetadataMask\NewsdayMetadataMask\src\main\custommetadata\newsdaymetadata.xml c:\Hermes11\custommetadata\
-pause
