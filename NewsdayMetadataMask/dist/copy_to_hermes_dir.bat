a2ab884fb2456f26fa6a861b1799ad53bb803726
jp <jp.mariano@newscycle.com>
2015-05-04 23:03:08 +0800

    convert to Maven project

--- NewsdayMetadataMask/dist/copy_to_hermes_dir.bat
+++
@@ -1,3 +0,0 @@
-copy C:\GitRepos\Newsday\NewsdayMetadataMask\NewsdayMetadataMask\dist\newsdaymetadata.jar c:\Hermes11\custommetadata\
-copy C:\GitRepos\Newsday\NewsdayMetadataMask\NewsdayMetadataMask\custommetadata\newsdaymetadata.xml c:\Hermes11\custommetadata\
-pause
---
+++ NewsdayMetadataMask/src/main/scripts/copy_to_hermes_dir.bat
@@ -0,0 +1,3 @@
+copy C:\GitRepos\Newsday\NewsdayMetadataMask\NewsdayMetadataMask\target\newsdaymetadata.jar c:\Hermes11\custommetadata\
+copy C:\GitRepos\Newsday\NewsdayMetadataMask\NewsdayMetadataMask\src\main\custommetadata\newsdaymetadata.xml c:\Hermes11\custommetadata\
+pause
